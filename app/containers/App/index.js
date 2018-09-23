dc3e00bf25207b5e8344287277878ba7d4156829
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2018-09-23 20:29:13 +0700

    icon

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -13,23 +13,19 @@ import 'antd/dist/antd.css';
 
 import { Switch, Route } from 'react-router-dom';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import LoginPage from 'containers/LoginPage/Loadable';
-import AdminPage from 'containers/AdminPage/Loadable';
-import TeacherPage from 'containers/TeacherPage/Loadable';
-import ParentPage from 'containers/ParentPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
+import HomePage from 'containers/HomePage';
+// import LoginPage from 'containers/LoginPage/Loadable';
+// import AdminPage from 'containers/AdminPage/Loadable';
+// import TeacherPage from 'containers/TeacherPage/Loadable';
+// import ParentPage from 'containers/ParentPage/Loadable';
+// import Header from 'components/Header';
+// import Footer from 'components/Footer';
 
-import {
-  userIsAuthenticatedRedir,
-  userIsNotAuthenticatedRedir,
-  userRedir,
-} from 'auth';
-
-const Login = userIsNotAuthenticatedRedir(LoginPage);
-const Admin = userIsAuthenticatedRedir(userRedir('Admin')(AdminPage));
-const Teacher = userIsAuthenticatedRedir(userRedir('Teacher')(TeacherPage));
-const Parent = userIsAuthenticatedRedir(userRedir('Parent')(ParentPage));
+// import {
+//   userIsAuthenticatedRedir,
+//   userIsNotAuthenticatedRedir,
+//   userRedir,
+// } from 'auth';
 
 const AppWrapper = styled.div`
   max-width: calc(768px + 16px * 2);
@@ -42,22 +38,19 @@ const AppWrapper = styled.div`
 
 const App = () => (
   <AppWrapper>
-    <Helmet
-      titleTemplate="%s - React.js Boilerplate"
-      defaultTitle="React.js Boilerplate"
-    >
-      <meta name="description" content="A React.js Boilerplate application" />
+    <Helmet titleTemplate="IQTrivia Homepage" defaultTitle="IQTrivia Homepage">
+      <meta name="description" content="IQTrivia Homepage" />
     </Helmet>
-    <Header />
+    {/* <Header /> */}
     <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="/login" component={Login} />
+      <Route exact path="/" component={HomePage} />
+      {/* <Route path="/login" component={Login} />
       <Route path="/admin" component={Admin} />
       <Route path="/teacher" component={Teacher} />
-      <Route path="/parent" component={Parent} />
+      <Route path="/parent" component={Parent} /> */}
       <Route path="" component={NotFoundPage} />
     </Switch>
-    <Footer />
+    {/* <Footer /> */}
   </AppWrapper>
 );
 
